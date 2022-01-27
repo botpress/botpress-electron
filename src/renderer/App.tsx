121a34acffb05f19f37a196ce0d631ef32bcbe59
Patrick <87815239+ptrckbp@users.noreply.github.com>
2022-01-27 15:47:31 -0500

    Cosmetic changes to loading screen

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,26 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import icon from '../../assets/bp-logo-white.png';
 import './App.css';
+import React from 'react';
 
-const Hello = () => {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-};
+import { BeatLoader } from 'react-spinners';
 
 export default function App() {
+  const [latestLog, setLatestLog] = React.useState("Launching Botpress");
+  React.useEffect(() => {
+    window.electron.ipcRenderer.on('botpress-instance-data', (arg) => {
+      setLatestLog(arg);
+      console.log("🚀 ~ file: App.tsx ~ line 12 ~ window.electron.ipcRenderer.on ~ arg", arg)
+    });
+  }, []);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
+    <div id="main-content">
+      <img width="315px" height="72px" alt="icon" src={icon} />
+      <div id="loading-icon">
+        <BeatLoader color="white" />
+      </div>
+      <p id="loading-text">{latestLog}</p>
+    </div>
   );
 }
