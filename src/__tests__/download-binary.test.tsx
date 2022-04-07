b817ecc75907fd8f669608b3838ad660711e5cb1
Patrick <87815239+ptrckbp@users.noreply.github.com>
2022-04-07 14:59:11 -0400

    adds ability to load nightlies by date

--- src/__tests__/download-binary.test.tsx
+++ src/__tests__/download-binary.test.tsx
@@ -1,13 +1,13 @@
 import https from 'https';
-import { botpressVersion } from '../../package.json';
+import { botpressVersion, nightlyDate } from '../../package.json';
 import buildUrl from '../main/binary-url-builder';
 
 const botpressOsTypes = ['darwin', 'win', 'linux'];
 
 describe('validating download links', () => {
   botpressOsTypes.forEach((botpressOsType) => {
-    test(`checking ${botpressOsType}`, async () => {
-      const url = buildUrl(botpressVersion, botpressOsType);
+    test(`checking ${botpressOsType} nightly date : ${nightlyDate}`, async () => {
+      const url = buildUrl(botpressVersion, botpressOsType, nightlyDate);
       const fileExists = await new Promise((resolve) => {
         https
           .get(url, (res) => {
--- src/main/binary-url-builder.ts
+++ src/main/binary-url-builder.ts
@@ -1,4 +1,11 @@
-const buildUrl = (downloadVersionTag: string, downloadOsTag: string) => {
+const buildUrl = (
+  downloadVersionTag: string,
+  downloadOsTag: string,
+  nightlyDate: string | null
+) => {
+  if (typeof nightlyDate === 'string') {
+    return `https://s3.ca-central-1.amazonaws.com/botpress-next-bins/nightly-${nightlyDate}/botpress-${downloadVersionTag}-${downloadOsTag}-x64.zip`;
+  }
   return `https://s3.amazonaws.com/botpress-binaries/botpress-${downloadVersionTag}-${downloadOsTag}-x64.zip`;
 };
 
